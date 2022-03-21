132e48a4d8a53f9320858d0a72c4ff195996a5b1
Murilo <inayievmpl@gmail.com>
2022-03-21 16:11:14 -0300

    backend: npm install cors - config cors in index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 // Variables
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
 const port = 8080
+
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use(
