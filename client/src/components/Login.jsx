91b714699df293f473a46d1838c5098c4b409697
Murilo <inayievmpl@gmail.com>
2022-03-21 16:33:58 -0300

    frontend: create component login.jsx

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login(props) {
+    return (
+        <div>
+            <h1>Login</h1>
+            <button onClick={props.handleTransition}>Logar</button>
+        </div>
+    )
+}
