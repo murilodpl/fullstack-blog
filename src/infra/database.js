6c03e4fa4940191a92712d2b50d0544fb847ce99
Murilo <inayievmpl@gmail.com>
2022-03-24 17:59:05 -0300

    backend database test

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,6 +1,6 @@
 const connectionString = 'postgres://postgres:1234@localhost:5432/he4rtbackend'
 
 const pgp = require('pg-promise')()
-const db = pgp({connectionString: process.env.DATABASE_URL || connectionString})
+const db = pgp({connectionString: 'postgres://qjkobsstlwdegy:d5d6622ffe477113576cd6dede0d0bde272865c24cc41d769eb5853d69da232c@ec2-18-215-96-22.compute-1.amazonaws.com:5432/dbbs71dkq024ab' || connectionString})
 
 module.exports = db
