b6c3e0b628ebd5a0edd2f81a1f42b9bffb82ce9d
Murilo <inayievmpl@gmail.com>
2022-03-24 17:04:03 -0300

    backend: update database info

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,9 +1,16 @@
 const pgp = require('pg-promise')()
+// const db = pgp({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'he4rtbackend',
+//     password: '1234',
+//     port: 5432,
+// })
 const db = pgp({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'he4rtbackend',
-    password: '1234',
+    user: 'qjkobsstlwdegy',
+    host: 'ec2-18-215-96-22.compute-1.amazonaws.com',
+    database: 'dbbs71dkq024ab',
+    password: 'd5d6622ffe477113576cd6dede0d0bde272865c24cc41d769eb5853d69da232c',
     port: 5432,
 })
 
