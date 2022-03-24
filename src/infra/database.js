cf483c758cf0f8edd0d5f524489ef8c01d09492d
Murilo <inayievmpl@gmail.com>
2022-03-24 17:08:52 -0300

    backend: fix database cred

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,16 +1,9 @@
 const pgp = require('pg-promise')()
-// const db = pgp({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'he4rtbackend',
-//     password: '1234',
-//     port: 5432,
-// })
 const db = pgp({
-    user: 'qjkobsstlwdegy',
-    host: 'ec2-18-215-96-22.compute-1.amazonaws.com',
-    database: 'dbbs71dkq024ab',
-    password: 'd5d6622ffe477113576cd6dede0d0bde272865c24cc41d769eb5853d69da232c',
+    user: 'postgres',
+    host: 'localhost',
+    database: 'he4rtbackend',
+    password: '1234',
     port: 5432,
 })
 
