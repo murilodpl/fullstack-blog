f4dcf0ce806eed957b80b767e727b1348e4f1129
Murilo <inayievmpl@gmail.com>
2022-03-31 16:57:37 -0300

    backend: update sql table - Create table posts

--- database/create.sql
+++ database/create.sql
@@ -12,3 +12,14 @@ insert into he4rtbackend.users (email, password, name, admin)
 values
     ('murilo@gmail.com', '123', 'Murilo', false),
     ('admin@gmail.com', '123', 'Admin', true);
+
+create table he4rtbackend.posts (
+    id serial primary key,
+    title text unique not null,
+    content text not null
+);
+
+insert into he4rtbackend.posts (title, content)
+values
+    ('Lorem 01', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    ('Lorem 02', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
