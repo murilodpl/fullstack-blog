8d22676e12164494e2df1fd73199b7d5acb7673c
Murilo <inayievmpl@gmail.com>
2022-03-31 17:00:26 -0300

    backend: update index.js - update routes structure - create 404 error

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ const serveStatic = require('serve-static')
 
 app.use(cors())
 app.use(express.json())
-app.use(serveStatic(__dirname + '/client/dist'))
+// app.use(serveStatic(__dirname + '/client/dist'))
 
-app.use('/', require('./src/route/usersRoute'))
+app.use('/users', require('./src/route/usersRoute'))
+app.use('/posts', require('./src/route/postsRoute'))
+
 app.use((error, req, res, next) => {
-    if (error.message === 'User already exists') {
-        return res.status(409).send({ status: 409, error: error.message })
-    }
     if (error.message === 'Email or password wrong') {
         return res.status(403).send({ status: 403, error: error.message })
+    }
+    if (error.message === 'Not found') {
+        return res.status(404).send({ status: 404, error: error.message })
+    }
+    if (error.message === 'Already exists') {
+        return res.status(409).send({ status: 409, error: error.message })
     }
     res.status(500).send({ status: 500, error: error.message })
 })
