fe29d97e7ea5beffa5c92c4e5c375e82ed4befab
Murilo <inayievmpl@gmail.com>
2022-03-31 18:03:59 -0300

    frontend: update Dashboard page - Get posts from database - Update post component   -- Add author id

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,8 +1,9 @@
 export default function Post(props) {
     return (
         <div className="border-b py-4">
-            <h3>{props.title}</h3>
-            <p>{props.content}</p>
+            <h3>{props.post.title}</h3>
+            <p>{props.post.content}</p>
+            <small>{props.post.author}</small>
         </div>
     )
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Header from "../components/Header";
-
+import Post from "../components/Post";
+import api from "../services/api"
 
 export default function Dashboard() {
     // Variables
     const { state } = useLocation(); // state { id: int, email: string, name: string, password: string, admin: boolean }
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
+
+    // Get posts
+    useEffect(() => {
+        async function getPosts() {
+            setIsLoading(true)
+            await api.get("/posts/getAll")
+                .then(res => {
+                    // console.log(res)
+                    setPosts(res.data)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+            setIsLoading(false)
+        }
+
+        getPosts();
+    }, [])
+
+    const postsElement = (posts.length != 0) ? posts.map((post, index) => <Post key={index} post={post} />) : "Não tem post"
 
     return (
         <div className="h-full">
@@ -13,7 +37,8 @@ export default function Dashboard() {
             <div className="container">
                 <h2 className="text-primary mb-4">Blog</h2>
 
-                
+                {(isLoading) ? 'Loading...'
+                    : (posts) && postsElement}
             </div>
         </div>
     )
