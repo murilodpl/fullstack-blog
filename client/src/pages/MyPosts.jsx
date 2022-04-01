a86c70c2b8c0a1f7fd84333eb2553110329ca1a1
Murilo <inayievmpl@gmail.com>
2022-04-01 16:16:00 -0300

    frontend: update MyPost page - Add new post btn

--- client/src/pages/MyPosts.jsx
+++ client/src/pages/MyPosts.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import Header from "../components/Header";
 import Post from "../components/Post";
 import api from "../services/api"
@@ -34,14 +35,17 @@ export default function Home(props) {
         return () => setIsLoading(false);
     }, [removePost])
 
-    const postsElement = (posts.length != 0) ? posts.map((post, index) => <Post key={index} post={post} setRemovePost={setRemovePost} />) : "Não tem post"
+    const postsElement = (posts.length != 0) ? posts.map((post, index) => <Post key={index} post={post} myposts={true} setRemovePost={setRemovePost} />) : "Não tem post"
 
     return (
         <div className="h-full">
             <Header user={props.user} />
 
             <div className="container">
-                <h2 className="text-primary">My Posts</h2>
+                <div className="flex justify-between items-center">
+                    <h2 className="text-primary">My Posts</h2>
+                    <NavLink to="/newPost" className="p-1 text-green-500 border border-green-500 transition-all hover:bg-green-500 hover:text-white" >New Post</NavLink>
+                </div>
 
                 {(isLoading) ? 'Loading...'
                     : (posts) && postsElement}
