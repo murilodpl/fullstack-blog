61f8027c7689f2acbfb8f76c5a5875c7560a19d1
Murilo <inayievmpl@gmail.com>
2022-04-01 16:15:33 -0300

    frontend: create new page NewPost

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home"
 import LoginPage from "./pages/LoginPage"
 import MyPosts from "./pages/MyPosts"
+import NewPost from "./pages/NewPost"
 import { Routes, Route, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
@@ -21,6 +22,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home user={user[0]} />} />
         <Route path="/myposts" element={<MyPosts user={user[0]} />} />
+        <Route path="/newPost" element={<NewPost user={user[0]} />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div >
---
+++ client/src/pages/NewPost.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react"
+import Header from "../components/Header";
+import api from "../services/api"
+import { useNavigate } from "react-router-dom";
+
+export default function NewPost(props) {
+    // Variable
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        title: "",
+        content: "",
+        author: props.user.id
+    })
+    const [check, setCheck] = useState({
+        title: false,
+        content: false,
+        isDuplicate: false,
+        err: false,
+        isLoading: false,
+        success: false
+    })
+
+    // Function
+    function handleChange(e) {
+        setCheck({
+            title: false,
+            content: false,
+            isDuplicate: false,
+            err: false,
+            isLoading: false
+        })
+
+        let { name, value } = e.target
+
+        setFormData(prevData => ({
+            ...prevData,
+            [name]: value
+        }))
+    }
+
+    function registerPost(e) {
+        e.preventDefault()
+
+        // If filled
+        if (formData.title == "") {
+            return setCheck(prevCheck => ({ ...prevCheck, title: true }))
+        } else {
+            setCheck(prevCheck => ({ ...prevCheck, title: false }))
+        }
+
+        if (formData.content == "") {
+            return setCheck(prevCheck => ({ ...prevCheck, content: true }))
+        } else {
+            setCheck(prevCheck => ({ ...prevCheck, content: false }))
+        }
+
+        setCheck(prevCheck => ({ ...prevCheck, isLoading: true, err: false }))
+
+        api.post("/posts/register", formData)
+            .then(res => {
+                if (res.data.status === 201) {
+                    // console.log(res.data)
+                    setFormData(prevFormData => ({ ...prevFormData, title: "", content: "" }))
+                    setCheck(prevCheck => ({ ...prevCheck, isLoading: false, success: true }))
+
+                    setTimeout(() => {
+                        navigate("/myposts");
+                    }, 1000);
+                } else {
+                    setCheck(prevCheck => ({ ...prevCheck, isLoading: false, isDuplicate: true, success: false }))
+                }
+            })
+            .catch(error => {
+                setCheck(prevCheck => ({ ...prevCheck, isLoading: false, err: true, success: false }))
+            })
+    }
+
+    return (
+        <div className="h-full">
+            <Header user={props.user} />
+
+            <div className="container">
+                <h2 className="text-primary">New Post</h2>
+
+                <form className="mt-4" name="registerPost" id="registerPost">
+                    <label htmlFor="title">Title:</label>
+                    <input className={`${(check.title || check.isDuplicate) && 'border-red-500'}`} type="text" id="title" name="title" value={formData.title} onChange={handleChange} required />
+
+                    <label htmlFor="content">Content:</label>
+                    <textarea className={`${(check.content || check.isDuplicate) && 'border-red-500'}`} id="content" name="content" value={formData.content} onChange={handleChange} required cols="30" rows="10"></textarea>
+
+
+                    {(check.isDuplicate) && <p className="text-red-500">Título duplicado...</p>}
+                    {(check.err) && <p className="text-red-500">Ocorreu algum erro, tente novamente mais tarde.</p>}
+                    {(check.isLoading) && <div className="flex justify-center"><div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>}
+                    {(check.success) && <p className="text-green-500">Cadastrado com sucesso!</p>}
+
+                    <button className="btnPost" type="submit" onClick={registerPost}>Submit</button>
+                </form>
+            </div>
+        </div>
+    )
+}
