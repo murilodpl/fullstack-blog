e446ed40733fa882266f70d1996d375fe87d7301
Murilo <inayievmpl@gmail.com>
2022-04-01 12:13:05 -0300

    frontend: update app.jsx - get user from localstorage - update routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
 import Home from "./pages/Home"
-import Dashboard from "./pages/Dashboard"
-import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage"
+import MyPosts from "./pages/MyPosts"
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [user, setUser] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("user") == null) {
+      return navigate("/login");
+    } else {
+      setUser(JSON.parse(localStorage.getItem("user"))[0])
+    }
+  }, [window.location.href]);
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Home user={user} />} />
+        <Route path="/myposts" element={<MyPosts user={user} />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div >
   )
