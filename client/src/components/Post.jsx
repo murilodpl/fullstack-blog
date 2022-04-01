84cbb97ba67e8173524ca899d74eb0e18ce6d211
Murilo <inayievmpl@gmail.com>
2022-04-01 14:56:37 -0300

    frontend: update post component - Add delete function

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,9 +1,27 @@
+import api from "../services/api"
+
 export default function Post(props) {
+    // Functions
+    async function deletePost(id) {
+        await api.delete('/posts/delete', { data: { id: id } })
+            .then(res => {
+                console.log(res)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            .finally(() => {
+                props.setRemovePost(prevBool => !prevBool)
+            })
+    }
+
     return (
-        <div className="border-b py-4">
+        <div className="border-b py-4 relative">
             <h3>{props.post.title}</h3>
             <p>{props.post.content}</p>
             <small>{props.post.author}</small>
+
+            <button onClick={() => deletePost(props.post.id)} className="absolute top-0 right-0 m-4 text-red-500 transition-all hover:text-red-700">Deletar</button>
         </div>
     )
 }
