5f1cc89a8322054527c8c0e203f969279b632d79
Murilo <mdpleopoldino@gmail.com>
2022-04-04 22:23:45 -0300

    frontend: update post component - add readmore btn

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,6 +1,10 @@
 import api from "../services/api"
+import { useState } from "react"
 
 export default function Post(props) {
+    // Variables
+    const [isReadMore, setIsReadMore] = useState(true)
+
     // Functions
     async function deletePost(id) {
         await api.delete('/posts/delete', { data: { id: id } })
@@ -14,6 +18,10 @@ export default function Post(props) {
                 props.setRemovePost(prevBool => !prevBool)
             })
     }
+    function toggleReadMore() {
+        setIsReadMore(!isReadMore)
+    }
+    
 
     return (
         <div className="border border-gray-300 p-4 my-4">
@@ -21,7 +29,13 @@ export default function Post(props) {
                 <h3>{props.post.title}</h3>
                 {(props.myposts || props.admin) && <button onClick={() => deletePost(props.post.id)} className="p-1 text-red-500 border border-red-500 transition-all hover:bg-red-500 hover:text-white">Delete</button>}
             </div>
-            <p className="whitespace-pre-line">{props.post.content}</p>
+            <p className="whitespace-pre-line">
+            {isReadMore ? text.slice(0, 150) : props.post.content}
+
+            <span onClick={toggleReadMore} className="cursor-pointer">
+                {isReadMore ? "...read more" : " show less"}
+            </span>
+            </p>
             <small>{props.post.author}</small>
         </div>
     )
